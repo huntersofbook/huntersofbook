6bd55b2d508b98aef8d1b84ad712ef1c40d1853a
productdevbook <hi@productdevbook.com>
2022-08-30 12:53:08 +0300

    fix: time-from hooks

--- packages/core/src/utils/date-fns/use-time-from-now-strict.ts
+++ packages/core/src/utils/date-fns/use-time-from-now-strict.ts
@@ -2,7 +2,7 @@ import { Ref, onMounted, onUnmounted, ref } from 'vue'
 import { formatDistanceStrict } from 'date-fns'
 import { localizedFormatDistanceStrict } from './localized-format-distance-strict'
 type I18n = Parameters<typeof formatDistanceStrict>
-export function useTimeFromNowStrict(date: Date | number, autoUpdate = 60000, i18n: string, options?: I18n[2]): Ref<string> {
+export function useTimeFromNowStrict(date: Date | number, autoUpdate = 60000, i18n: string, options?: I18n[2], hook?: false): Ref<string> {
   const interval = ref(0)
 
   const formatOptions = {
@@ -12,18 +12,19 @@ export function useTimeFromNowStrict(date: Date | number, autoUpdate = 60000, i1
 
   const formattedDate = ref(localizedFormatDistanceStrict(i18n, date, new Date(), formatOptions))
 
-  onMounted(() => {
-    if (autoUpdate !== 0) {
-      interval.value = window.setInterval(() => {
-        formattedDate.value = localizedFormatDistanceStrict(i18n, date, new Date(), formatOptions)
-      }, autoUpdate)
-    }
-  })
+  if (hook) {
+    onMounted(() => {
+      if (autoUpdate !== 0) {
+        interval.value = window.setInterval(() => {
+          formattedDate.value = localizedFormatDistanceStrict(i18n, date, new Date(), formatOptions)
+        }, autoUpdate)
+      }
+    })
 
-  onUnmounted(() => {
-    if (autoUpdate !== 0)
-      clearInterval(interval.value)
-  })
-
+    onUnmounted(() => {
+      if (autoUpdate !== 0)
+        clearInterval(interval.value)
+    })
+  }
   return formattedDate
 }
--- packages/core/src/utils/date-fns/use-time-from-now.ts
+++ packages/core/src/utils/date-fns/use-time-from-now.ts
@@ -2,7 +2,7 @@ import { Ref, onMounted, onUnmounted, ref } from 'vue'
 import { formatDistance } from 'date-fns'
 import { localizedFormatDistance } from './localized-format-distance'
 type I18n = Parameters<typeof formatDistance>
-export function useTimeFromNow(date: Date | number, autoUpdate = 60000, i18n: string, options?: I18n[2]): Ref<string> {
+export function useTimeFromNow(date: Date | number, autoUpdate = 60000, i18n: string, options?: I18n[2], hook?: false): Ref<string> {
   const interval = ref(0)
 
   const formatOptions = {
@@ -12,18 +12,20 @@ export function useTimeFromNow(date: Date | number, autoUpdate = 60000, i18n: st
 
   const formattedDate = ref(localizedFormatDistance(i18n, date, new Date(), formatOptions))
 
-  onMounted(() => {
-    if (autoUpdate !== 0) {
-      interval.value = window.setInterval(() => {
-        formattedDate.value = localizedFormatDistance(i18n, date, new Date(), formatOptions)
-      }, autoUpdate)
-    }
-  })
+  if (hook) {
+    onMounted(() => {
+      if (autoUpdate !== 0) {
+        interval.value = window.setInterval(() => {
+          formattedDate.value = localizedFormatDistance(i18n, date, new Date(), formatOptions)
+        }, autoUpdate)
+      }
+    })
 
-  onUnmounted(() => {
-    if (autoUpdate !== 0)
-      clearInterval(interval.value)
-  })
+    onUnmounted(() => {
+      if (autoUpdate !== 0)
+        clearInterval(interval.value)
+    })
+  }
 
   return formattedDate
 }
