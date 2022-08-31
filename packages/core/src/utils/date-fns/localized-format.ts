2834d3690ba643e07441c8b633437a1c3d0316ac
productdevbook <hi@productdevbook.com>
2022-08-31 14:49:17 +0300

    fix: localizedFormat

--- packages/core/src/utils/date-fns/localized-format.ts
+++ packages/core/src/utils/date-fns/localized-format.ts
@@ -3,8 +3,8 @@ import { getDateFNSLocale } from './get-date-fns-locale'
 
 export type LocalizedFormat = (i18n?: string | undefined, ...a: Parameters<typeof format>) => string
 
-export const localizedFormat: LocalizedFormat = (i18n, date, format: any, options): string => {
-  return format(date, format, {
+export const localizedFormat: LocalizedFormat = (i18n, date, formatDate, options): string => {
+  return format(date, formatDate, {
     ...options,
     locale: getDateFNSLocale(i18n),
   })
