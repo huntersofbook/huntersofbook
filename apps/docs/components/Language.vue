2b9deae6217be2c2ed4887c81ce00d07bcb1c79f
productdevbook <hi@productdevbook.com>
2022-09-02 08:24:56 +0300

    fix: color

--- apps/docs/components/Language.vue
+++ apps/docs/components/Language.vue
@@ -21,19 +21,19 @@ const changeLanguage = async (value: string) => {
 
 <template>
   <div class="relative box-border">
-    <button class="text-sm font-medium p-3 flex items-center justify-center hover:bg-stone-700 rounded" @click="isOpen = !isOpen">
+    <button class="text-sm font-medium p-3 flex items-center justify-center hover:bg-gray-200 dark:hover:bg-stone-700 rounded" @click="isOpen = !isOpen">
       <div class="i-ph-translate-bold text-lg mr-2" />
       {{ locale }}
     </button>
     <div
       v-if="isOpen"
 
-      class="absolute top-0 left-0 mt-12 bg-stone-500 rounded w-20 p-1"
+      class="absolute top-0 left-0 mt-12 bg-gray-200 dark:bg-stone-500 rounded w-20 p-1"
     >
       <button
         v-for="language in languages"
         :key="language.value"
-        class="px-3 py-2 flex w-full items-center justify-center hover:bg-stone-700 rounded"
+        class="px-3 py-2 flex w-full items-center hover:bg-gray-400 justify-center dark:hover:bg-stone-700 rounded"
         @click="changeLanguage(language.value)"
       >
         {{ language.label }}
