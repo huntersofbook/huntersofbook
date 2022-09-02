cd06c44b735778ec0516d9fe64801d7cc6528559
Mehmet <hi@productdevbook.com>
2022-09-02 08:27:14 +0300

    feat: fix/vite plugin i18n (#23)
    * core: upgrade vite-plugin i18n
    
    * fix: change button
    
    * fix: docs
    
    * fix: workspace
    
    * fix: color

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: [
-    "./components/**/*.{vue,js,ts,jsx,tsx}",
-    "./docs/**/*.{vue,js,ts,jsx,tsx}",
+    './components/**/*.{vue,js,ts,jsx,tsx}',
+    './docs/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
