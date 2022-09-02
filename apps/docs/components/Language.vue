5c92aa322573df0d82ef498d4c49a6e8be857e8a
productdevbook <hi@productdevbook.com>
2022-09-02 08:07:20 +0300

    fix: change button

--- apps/docs/components/Language.vue
+++ apps/docs/components/Language.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
 import { loadDateFNSLocale } from 'huntersofbook'
 import { useI18n } from 'vue-i18n'
+import { ref } from 'vue'
 const { locale, availableLocales } = useI18n()
+
+const isOpen = ref(false)
 
 const languages = availableLocales.map(locale => ({
   label: locale,
   value: locale,
 }))
+
 const changeLanguage = async (value: string) => {
   await loadDateFNSLocale(value).then((res) => {
-    console.log(res)
+    locale.value = value
+    isOpen.value = false
   })
-  locale.value = value
 }
 </script>
 
 <template>
   <div class="relative box-border">
-    <button class="text-sm font-medium p-3 flex items-center justify-center hover:bg-stone-700 rounded">
+    <button class="text-sm font-medium p-3 flex items-center justify-center hover:bg-stone-700 rounded" @click="isOpen = !isOpen">
       <div class="i-ph-translate-bold text-lg mr-2" />
       {{ locale }}
     </button>
     <div
-      class="absolute top-0 left-0 mt-12 bg-stone-500 rounded w-20"
+      v-if="isOpen"
+
+      class="absolute top-0 left-0 mt-12 bg-stone-500 rounded w-20 p-1"
     >
       <button
         v-for="language in languages"
         :key="language.value"
-        class="p-3 flex items-center justify-center hover:bg-stone-700 rounded"
+        class="px-3 py-2 flex w-full items-center justify-center hover:bg-stone-700 rounded"
         @click="changeLanguage(language.value)"
       >
         {{ language.label }}
