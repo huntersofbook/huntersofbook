9db0b4c5f5c802640cc42083d13e8209b215aa7e
Mehmet <hi@productdevbook.com>
2022-09-19 12:03:00 +0300

    fix: naive ui auto component (#41)
    * fix: naive-nuxt auto import
    
    * console.log delete
    
    * Update module.ts
    
    * Update module.ts

--- packages/naive-ui-nuxt/src/module.ts
+++ packages/naive-ui-nuxt/src/module.ts
@@ -1,33 +1,13 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import {
+  addComponent,
   addImportsSources,
   addPlugin,
-  addVitePlugin,
   createResolver,
   defineNuxtModule
 } from '@nuxt/kit'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
 
 import { name, version } from '../package.json'
-
-const NaiveUIHooks = [
-  'darkTheme',
-  'lightTheme',
-  'useOsTheme',
-  'NMessageProvider',
-  'NConfigProvider',
-  'NLoadingBarProvider',
-  'NDialogProvider',
-  'NNotificationProvider',
-  'useDialog',
-  'useMessage',
-  'useNotification',
-  'useLoadingBar',
-  'createTheme',
-  'inputDark',
-  'datePickerDark',
-  'GlobalThemeOverrides'
-]
 
 export default defineNuxtModule({
   meta: {
@@ -64,15 +44,16 @@ export default defineNuxtModule({
     })
 
     nuxt.options.head.meta.push({ name: 'naive-ui-style' })
+    addPlugin({ src: resolve('./runtime/plugin'), mode: 'server' })
 
-    addVitePlugin(
-      Components({
-        dts: resolve(nuxt.options.buildDir, 'types/naive-ui.d.ts'),
-        resolvers: [NaiveUiResolver()]
+    // Add auto-imported components
+    NaiveComponentNames.map((name) =>
+      addComponent({
+        name,
+        export: name,
+        filePath: 'naive-ui'
       })
     )
-
-    addPlugin({ src: resolve('./runtime/plugin'), mode: 'all' })
 
     addImportsSources([
       {
@@ -82,3 +63,164 @@ export default defineNuxtModule({
     ])
   }
 })
+
+const NaiveComponentNames = [
+  'NA',
+  'NAffix',
+  'NAlert',
+  'NAnchor',
+  'NAnchorLink',
+  'NAutoComplete',
+  'NAvatar',
+  'NAvatarGroup',
+  'NBackTop',
+  'NBadge',
+  'NBlockquote',
+  'NBreadcrumb',
+  'NBreadcrumbItem',
+  'NButton',
+  'NButtonGroup',
+  'NCalendar',
+  'NCard',
+  'NCarousel',
+  'NCarouselItem',
+  'NCascader',
+  'NCheckbox',
+  'NCheckboxGroup',
+  'NCode',
+  'NCol',
+  'NCollapse',
+  'NCollapseItem',
+  'NCollapseTransition',
+  'NColorPicker',
+  'NConfigProvider',
+  'NCountdown',
+  'NDataTable',
+  'NDatePicker',
+  'NDescriptions',
+  'NDescriptionsItem',
+  'NDialog',
+  'NDialogProvider',
+  'NDivider',
+  'NDrawer',
+  'NDrawerContent',
+  'NDropdown',
+  'NDynamicInput',
+  'NDynamicTags',
+  'NEl',
+  'NElement',
+  'NEllipsis',
+  'NEmpty',
+  'NForm',
+  'NFormItem',
+  'NFormItemCol',
+  'NFormItemGi',
+  'NFormItemGridItem',
+  'NFormItemRow',
+  'NGi',
+  'NGlobalStyle',
+  'NGradientText',
+  'NGrid',
+  'NGridItem',
+  'NH1',
+  'NH2',
+  'NH3',
+  'NH4',
+  'NH5',
+  'NH6',
+  'NHr',
+  'NIcon',
+  'NIconWrapper',
+  'NImage',
+  'NImageGroup',
+  'NInput',
+  'NInputGroup',
+  'NInputGroupLabel',
+  'NInputNumber',
+  'NLayout',
+  'NLayoutContent',
+  'NLayoutFooter',
+  'NLayoutHeader',
+  'NLayoutSider',
+  'NLegacyTransfer',
+  'NLi',
+  'NList',
+  'NListItem',
+  'NLoadingBarProvider',
+  'NLog',
+  'NMention',
+  'NMenu',
+  'NMessageProvider',
+  'NModal',
+  'NNotificationProvider',
+  'NNumberAnimation',
+  'NOl',
+  'NP',
+  'NPageHeader',
+  'NPagination',
+  'NPopconfirm',
+  'NPopover',
+  'NPopselect',
+  'NProgress',
+  'NRadio',
+  'NRadioButton',
+  'NRadioGroup',
+  'NRate',
+  'NResult',
+  'NRow',
+  'NScrollbar',
+  'NSelect',
+  'NSkeleton',
+  'NSlider',
+  'NSpace',
+  'NSpin',
+  'NStatistic',
+  'NStep',
+  'NSteps',
+  'NSwitch',
+  'NTab',
+  'NTabPane',
+  'NTable',
+  'NTabs',
+  'NTag',
+  'NTbody',
+  'NTd',
+  'NText',
+  'NTh',
+  'NThead',
+  'NThing',
+  'NTime',
+  'NTimePicker',
+  'NTimeline',
+  'NTimelineItem',
+  'NTooltip',
+  'NTr',
+  'NTransfer',
+  'NTree',
+  'NTreeSelect',
+  'NUl',
+  'NUpload',
+  'NUploadDragger',
+  'NUploadFileList',
+  'NUploadTrigger',
+  'NWatermark'
+]
+
+const NaiveUIHooks = [
+  'darkTheme',
+  'lightTheme',
+  'useOsTheme',
+  'NMessageProvider',
+  'NConfigProvider',
+  'NLoadingBarProvider',
+  'NDialogProvider',
+  'NNotificationProvider',
+  'useDialog',
+  'useMessage',
+  'useNotification',
+  'useLoadingBar',
+  'createTheme',
+  'inputDark',
+  'datePickerDark',
+  'GlobalThemeOverrides'
+]
