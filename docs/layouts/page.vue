1c148a89e355fc7ca907afe39ace4876db69c5e2
productdevbook <hi@productdevbook.com>
2022-09-21 12:24:06 +0300

    docs: click tranckEvent

--- docs/layouts/page.vue
+++ docs/layouts/page.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const { page } = useContent()
+const { trackEvent } = usePlausible()
 </script>
 
 <template>
@@ -14,6 +15,7 @@ const { page } = useContent()
         href="https://github.com/sponsors/productdevbook"
         target="_blank"
         class="fixed px-4 hover:bg-red-400 flex shadow shadow-red-100 font-semibold items-center justify-center bottom-14 right-10 py-2 bg-red-500 row-span-full text-lg text-white rounded-full"
+        @click="trackEvent('sponsor')"
       >
         <Icon name="uil:github" size="24" class="mr-2" />
         <p>sponsor us</p>
