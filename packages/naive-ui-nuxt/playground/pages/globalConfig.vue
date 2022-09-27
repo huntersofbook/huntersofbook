225d3893ba70ec608ee69afc93e79b96e955bb60
Tobias Diez <code@tobiasdiez.com>
2022-09-27 08:43:20 +0200

    feat: support global config via module options in nuxt config (#52)
    * feat: support global config via module options in nuxt config
    
    * docs: add naive ui theme-overrides (#1)
    
    Co-authored-by: Mehmet <hi@productdevbook.com>

--- packages/naive-ui-nuxt/playground/pages/globalConfig.vue
+++ packages/naive-ui-nuxt/playground/pages/globalConfig.vue
@@ -1,14 +1,11 @@
-<script setup lang="ts">
-import { darkTheme } from 'naive-ui'
-</script>
-
 <template>
-  <NConfigProvider :theme="darkTheme">
-    <NGlobalStyle />
+  <div>
+    Displays controls in the style determined by the config in
+    <code>nuxt.config.ts</code>.
     <div class="mx-auto mt-4 max-w-sm">
       <NButton>Default</NButton>
       <NButton>Default</NButton>
       <NInput>Default</NInput>
     </div>
-  </NConfigProvider>
+  </div>
 </template>
