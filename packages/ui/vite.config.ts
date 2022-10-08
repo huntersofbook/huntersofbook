3096bbae41feb1f9f2ce2b714085c2bc794c3e48
productdevbook <hi@productdevbook.com>
2022-10-08 15:12:27 +0300

    fix: macros

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,8 +1,6 @@
 import path, { resolve } from 'path'
 
 import vue from '@vitejs/plugin-vue'
-import VueJsx from '@vitejs/plugin-vue-jsx'
-import VueMacros from 'unplugin-vue-macros/vite'
 import { defineConfig } from 'vite'
 import dtsPlugin from 'vite-plugin-dts'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
@@ -16,12 +14,7 @@ const externals = [
 export default defineConfig({
   plugins: [
     vue(),
-    VueMacros({
-      plugins: {
-        vue: vue(),
-        vueJsx: VueJsx(), // if needed
-      },
-    }),
+
     vueSetupExtend(),
     dtsPlugin({
       outputDir: 'dist/types',
