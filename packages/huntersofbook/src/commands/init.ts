4a412eba9c7e303262b6e0f2f11917211370a11f
productdevbook <hi@productdevbook.com>
2022-11-01 13:18:35 +0300

    add ora

--- packages/huntersofbook/src/commands/init.ts
+++ packages/huntersofbook/src/commands/init.ts
@@ -2,6 +2,8 @@ import fs from 'node:fs'
 
 import clear from 'clear'
 import consola from 'consola'
+import ora from 'ora'
+import { debounce } from 'perfect-debounce'
 
 import { defineHuntersofbookCommand } from '.'
 
@@ -31,12 +33,13 @@ export default defineHuntersofbookConfig({
 })
 `
 
+    clear()
+    const spinner = ora('Loading create huntersofbook.config.ts').start()
     // Resolve rootDir
     // const rootDir = resolve(args._[0] || '.')
-    clear()
-    consola.info('Init Huntersofbook')
-    fs.writeFileSync('huntersofbook.config.ts', code)
-    consola.success('Created huntersofbook.config.ts')
+    const a = debounce(() => fs.writeFileSync('huntersofbook.config.ts', code), 500)
+    await a()
+    spinner.succeed()
   },
 })
 
