05f7f44c8bad2fada931b559be9522e1e03f22a0
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2022-11-20 21:25:57 +0200

    refactor: re-work on merge function

--- packages/schob/src/merge.ts
+++ packages/schob/src/merge.ts
@@ -1,35 +1,52 @@
-import { pick } from 'filter-anything'
-import { isArray } from 'is-what'
-import { merge as mer, mergeAndCompare } from 'merge-anything'
+import { isArray, isObject } from 'is-what'
+import _ from 'lodash'
+import { mergeAndCompare } from 'merge-anything'
 import { removeProp } from 'remove-anything'
 
 import { Merger } from './types'
 
-function concatStrings(originVal: any, newVal: any, _key: any) {
+/**
+ * compare values
+ *
+ * @param originVal - the value in the schema
+ * @param newVal - the value in the new data
+ * @param _key - the key of the value
+ * @returns
+ */
+function compare(originVal: any, newVal: any, _key: any) {
   if (isArray(originVal) && isArray(newVal)) {
-    // concat & merge logic
+    // // if both arrays have no objects, we can just return the new array
+    if (arrayHasNoObject(originVal) && arrayHasNoObject(newVal)) {
+      const res = [...originVal, ...newVal]
+      return Array.from(new Set(res)) // return only unique values in an array
+    }
+    else {
+      // get unique non-object values
+      const nonObjectArrayNewVal = originVal.filter(item => !isObject(item))
+      const nonObjectArrayOriginVal = newVal.filter(item => !isObject(item))
+      const nonObjectArray = Array.from(new Set([...nonObjectArrayNewVal, ...nonObjectArrayOriginVal])) // unique non-object values
 
-    const overlappingPart = originVal.slice(0, newVal.length)
+      // get unique object values
+      const objectArrayNewVal = originVal.filter(isObject)
+      const objectArrayOriginVal = newVal.filter(isObject)
+      const objectArray = _.uniq([...objectArrayNewVal, ...objectArrayOriginVal]) // unique object values
 
-    const data = overlappingPart
-      .map((p, i) => {
-        const data = newVal[i] ? mer(p, newVal[i]) : p
-        const schemaDefaultKeys = Object.keys(p)
-        const dd = pick(data, schemaDefaultKeys)
-        return dd
-      })
-      .concat(newVal.length > originVal.length ? originVal.slice(newVal.length) : newVal.slice(originVal.length))
-
-    return data
+      return [...nonObjectArray, ...objectArray]
+    }
   }
   else {
+    // if the key from newData has no corresponding key in schema, we don't want to merge it
     if (originVal !== undefined)
       return newVal
   }
 }
 
+function arrayHasNoObject(array: Array<any>) {
+  return array.every(item => !isObject(item))
+}
+
 export function merge(merge: Merger) {
-  const data = mergeAndCompare(concatStrings, merge.schema, merge.newData)
+  const data = mergeAndCompare(compare, merge.schema, merge.newData)
   const noUndefined = removeProp(data, undefined)
   return noUndefined
 }
