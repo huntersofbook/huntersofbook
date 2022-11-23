3543694e53da52d7b58e0e84c646eafcfcf7cd90
productdevbook <hi@productdevbook.com>
2022-11-23 21:20:51 +0300

    chore: docs

--- docs/components/LanguageChange.vue
+++ docs/components/LanguageChange.vue
@@ -8,18 +8,16 @@ function changeLocale(data) {
   loading.value = true
   setLocale(data)
   setTimeout(() => {
-    if (window) {
-      window.location.reload()
+    if (window)
       loading.value = false
-    }
   }, 300)
 }
 </script>
 
 <template>
   <div class="flex">
-    <button v-for="item in availableLocales" :key="item.code" :class="locale === item.code ? 'bg-blue-400' : 'bg-gray-200'" class="flex items-center justify-center relative rounded-lg p-2" @click.prevent.stop="changeLocale(item.code)">
-      <div v-if="loading" class="absolute w-full h-full flex items-center justify-center bg-black opacity-50">
+    <button v-for="item in availableLocales" :key="item.code" :class="locale === item.code ? 'bg-green-500' : 'bg-gray-200 dark:bg-gray-500'" class="flex items-center justify-center relative rounded-lg p-2" @click.prevent.stop="changeLocale(item.code)">
+      <div v-if="loading" class="absolute w-full h-full flex items-center justify-center bg-black dark:bg-gray-700 opacity-50">
         <Icon name="ph:circle-notch-bold" size="24" class="animate-spin " />
       </div>
       <span class="px-2">{{
