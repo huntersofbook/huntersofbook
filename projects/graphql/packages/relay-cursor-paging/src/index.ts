3b44a0366096e20ebbc88f3a1e50875e5954fe81
productdevbook <hi@productdevbook.com>
2022-12-08 10:19:46 +0300

    feat: relay-cursor-paging

---
+++ projects/graphql/packages/relay-cursor-paging/src/index.ts
@@ -0,0 +1,3 @@
+export { offsetForArgs } from './relay.js'
+export type { DefaultConnectionArguments, MaybePromise, ResolveCursorConnectionArgs, ResolveCursorConnectionOptions, ResolveOffsetConnectionOptions } from './types.js'
+
