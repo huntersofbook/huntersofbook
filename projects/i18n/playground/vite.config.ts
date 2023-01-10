9894362f12b6efeeca682978fabd06176da8784d
productdevbook <hi@productdevbook.com>
2023-01-10 20:27:33 +0300

    cover add

--- projects/i18n/playground/vite.config.ts
+++ projects/i18n/playground/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import unpluginI18n from '@huntersofbook/i18n/vite'
+import i18n from '@huntersofbook/i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    unpluginI18n({
-      languages: ['en', 'tr', 'fr'],
+    i18n({
+      languages: ['tr', 'en', 'ch'],
     }),
   ],
 })
