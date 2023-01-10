3c1f79555e9172473fb8949a0f102f5c989f7903
productdevbook <hi@productdevbook.com>
2023-01-10 18:09:10 +0300

    change name

--- projects/i18n/playground/vite.config.ts
+++ projects/i18n/playground/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import unpluginI18n from '@huntersofbook/unplugin-i18n-watch/vite'
+import unpluginI18n from '@huntersofbook/i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
