3fae05ddbbcd23ed7d658c13bde9c3a07b97cc9c
productdevbook <hi@productdevbook.com>
2023-01-28 17:11:18 +0300

    feat(naive-ui-nuxt): nuxt 3.1 ugrade and v0.6.0 publish

--- projects/naive-ui-nuxt/src/module.ts
+++ projects/naive-ui-nuxt/src/module.ts
@@ -21,7 +21,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: 'naiveUI',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '^3.1.1',
     },
   },
   setup(moduleOptions, nuxt) {
