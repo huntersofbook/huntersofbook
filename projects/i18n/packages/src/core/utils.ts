cd47c3707a756b79a448fd2e979a711fe91915bf
productdevbook <hi@productdevbook.com>
2023-01-29 18:15:56 +0300

    feat: directories save

--- projects/i18n/packages/src/core/utils.ts
+++ projects/i18n/packages/src/core/utils.ts
@@ -1,5 +1,6 @@
 import minimatch from 'minimatch'
 import { slash } from '@antfu/utils'
+import { isArray } from 'lodash'
 import { DISABLE_COMMENT } from './constants'
 
 export function matchGlobs(filepath: string, globs: string[]) {
@@ -14,4 +15,39 @@ export function shouldTransform(code: string) {
   if (code.includes(DISABLE_COMMENT))
     return false
   return true
+}
+
+/**
+       * It sorts the keys of an object, and if the value of a key is an object, it recursively sorts
+       * that object's keys as well
+       * thank you https://stackoverflow.com/a/72998623/17764989
+       */
+export const sortObject = (obj: any) => {
+  const sorted = Object.keys(obj)
+    .sort()
+    .reduce((accumulator: any, key: any) => {
+      if (typeof obj[key] === 'object') {
+        // recurse nested properties that are also objects
+        if (obj[key] == null) {
+          accumulator[key] = null
+        }
+        else if (isArray(obj[key])) {
+          accumulator[key] = obj[key].map((item: any) => {
+            if (typeof item === 'object')
+              return sortObject(item)
+
+            else
+              return item
+          })
+        }
+        else {
+          accumulator[key] = sortObject(obj[key])
+        }
+      }
+      else {
+        accumulator[key] = obj[key]
+      }
+      return accumulator
+    }, {})
+  return sorted
 }
