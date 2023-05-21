14d73c7b80435b41205a6b260ece52a06d1407cc
productdevbook <hi@productdevbook.com>
2023-05-21 06:50:56 +0300

    feat: delete schob

--- projects/schob/packages/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { merge } from './merge'
-
-export { merge }
-
--- projects/schob/packages/src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Input = Record<string | number | symbol, any>
-
-export interface Merger {
-  schema: Input
-  newData: Input
-}
